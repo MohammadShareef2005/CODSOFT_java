00b1e98eff8d5868c647ae56d63471b156391f62
MohammadShareef2005 <155538583+MohammadShareef2005@users.noreply.github.com>
2024-01-07 10:28:00 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -62,6 +62,50 @@ class ATM {
 }
 
 
+____________________________________________________________________________________________________________________________________________________________________
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if(amount > 0) {
+            balance += amount;
+            System.out.println("Money Deposited: " + amount);
+        } else {
+            System.out.println("Invalid amount for deposit!");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if(amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Money Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount for withdrawal!");
+        }
+    }
+}
+
+
+
+__________________________________________________________________________________________________________________________________________________________________
+
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000);
+        ATM atm = new ATM(userAccount);
+        atm.start();
+    }
+}
 
 
 
